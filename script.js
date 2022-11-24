a9be027d6e5d08137c12f5c8ad5b265a94387fc0
LuizaCloss22 <108022672+LuizaCloss22@users.noreply.github.com>
2022-11-24 20:08:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var i = setInterval(function (){
+    clearInterval(i);
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("content").style.display = "block";
+}, 2000);
